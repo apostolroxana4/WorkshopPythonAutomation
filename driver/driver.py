b627abba416ce69633e5690bd1eb4af6ffe4eb74
Razvan Moisa <razvan.moisa@fortech.ro>
2023-03-09 15:59:50 +0200

    Created a test that opens the page and accesses a link in 4 different methods

--- driver/driver.py
+++ driver/driver.py
@@ -1,2 +1,2 @@
-# Here ww will add the driver
-# https://the-internet.herokuapp.com/
+class Driver:
+    URL = "https://the-internet.herokuapp.com/"
--- tests/test_heroku.py
+++ tests/test_heroku.py
@@ -1,4 +1,17 @@
+import time
+
+from selenium.webdriver.common.by import By
+
 from driver.driver import Driver
 
 def test_open_page(browser):
     browser.get(Driver.URL)
+
+    # element = browser.find_element(By.LINK_TEXT, 'Shadow DOM') #by the text Shadow DOM
+    # element = browser.find_element(By.CSS_SELECTOR, 'a[href="/key_presses"]') #by the anchor with a specific attribute
+    # element = browser.find_element(By.XPATH, '/html/body/div[2]/div/ul/li[18]/a') #xpath
+    element = browser.find_element(By.CSS_SELECTOR, "li:nth-child(9) a") #the anchor in the 9th li
+    element.click()
+
+
+    time.sleep(5)
