98f2175faefdeea793c4a81759483572e29dccb8
andreea.cosma <a.cosma@specialized.com>
2023-03-13 11:12:22 +0200

    Find elements by Xpath

--- tests/test_herokuapp.py
+++ tests/test_herokuapp.py
@@ -1,6 +1,41 @@
+import time
+from selenium.webdriver.common.by import By
 from driver.driver import Driver
 
 def test_open_herokuapp_page(browser):
     browser.get(Driver.URL)
 
-    btnDropdown = browser.find_element('xpath', )
+    buttondropdown = browser.find_element(By.XPATH, '//*[@id="content"]/ul/li[11]/a') #relative
+    buttondropdown.click()
+    time.sleep(5)
+
+    buttoncontextmenu = browser.find_element(by=By.XPATH, value='//*[@id="content"]/ul/li[7]/a')
+    buttoncontextmenu.click()
+    time.sleep(5)
+
+    buttondragdrop = browser.find_element(by=By.XPATH, value='//*[@id="content"]/ul/li[10]/a')
+    buttondragdrop.click()
+    time.sleep(5)
+
+    buttonforgotpassword = browser.find_element(by=By.XPATH, value='//*[@id="content"]/ul/li[20]/a')
+    buttonforgotpassword.click()
+    time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
