4b3ad0977a7317f9f2553312a15f4713149d6ab0
Razvan Moisa <razvan.moisa@fortech.ro>
2023-03-24 16:46:21 +0200

    added check_home_title and check_documents_title functions for verification

--- pages/elements/methods.py
+++ pages/elements/methods.py
@@ -5,12 +5,24 @@ from pages.elements.items import items
 class Elements:
 
     @staticmethod
+    def check_checkbox(browser):
+        return browser.find_element(By.XPATH, items.check_box).is_displayed()
+
+    @staticmethod
     def click_checkbox(browser):
         browser.find_element(By.XPATH, items.check_box).click()
 
     @staticmethod
+    def check_home_title(browser):
+        return browser.find_element(By.XPATH, items.home_title).is_displayed()
+
+    @staticmethod
     def click_expand_home(browser):
         browser.find_element(By.CSS_SELECTOR, items.home_arrow).click()
+
+    @staticmethod
+    def check_documents_title(browser):
+        return browser.find_element(By.XPATH, items.documents_title).is_displayed()
 
     @staticmethod
     def click_documents(browser):
