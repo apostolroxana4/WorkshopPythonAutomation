b7056768306e6647e7158cd924184b0fba7d0176
Razvan Moisa <razvan.moisa@fortech.ro>
2023-04-03 18:02:42 +0300

    added a 3rd method, implemented the usefixtures mark, changed browser to driver as parameter and removed teardown

--- tests/elements/test_text_box.py
+++ tests/elements/test_text_box.py
@@ -1,10 +1,10 @@
+import pytest
 from driver.driver import Driver
 from pages.elements.methods import Elements
 
 '''
 This method uses conftest, to get the browser fixture as a parameter
 '''
-
 
 # # method 1
 # class TestTextBox:
@@ -24,25 +24,51 @@ This method uses browser_initialization function from driver, while skipping con
 It also closes the browser as a last step, since we can't do that with browser function within conftest
 '''
 
-
 # method 2
+# class TestTextBox:
+#     browser = Driver.browser_initialization()
+#
+#     def test_select_checkbox(self):
+#         assert Elements().check_checkbox(self.browser)
+#         Elements().click_checkbox(self.browser)
+#
+#     def test_expand_home(self):
+#         assert Elements().check_home_title(self.browser)
+#         Elements().click_expand_home(self.browser)
+#
+#     def test_click_documents(self):
+#         assert Elements().check_documents_title(self.browser)
+#         Elements().click_documents(self.browser)
+#
+#     def test_check_results(self):
+#         assert Elements().check_documents(self.browser)
+#
+#     def test_teardown(self):
+#         self.browser.quit()
+
+
+'''
+This method uses conftest and splits each action into a test
+check this for future reminders
+https://www.seleniumeasy.com/python/webdriver-tests-in-pytest-using-fixtures-and-conftest
+'''
+
+
+# method 3
+@pytest.mark.usefixtures("browser")
 class TestTextBox:
-    browser = Driver.browser_initialization()
 
     def test_select_checkbox(self):
-        assert Elements().check_checkbox(self.browser)
-        Elements().click_checkbox(self.browser)
+        assert Elements().check_checkbox(self.driver)
+        Elements().click_checkbox(self.driver)
 
     def test_expand_home(self):
-        assert Elements().check_home_title(self.browser)
-        Elements().click_expand_home(self.browser)
+        assert Elements().check_home_title(self.driver)
+        Elements().click_expand_home(self.driver)
 
     def test_click_documents(self):
-        assert Elements().check_documents_title(self.browser)
-        Elements().click_documents(self.browser)
+        assert Elements().check_documents_title(self.driver)
+        Elements().click_documents(self.driver)
 
     def test_check_results(self):
-        assert Elements().check_documents(self.browser)
-
-    def test_teardown(self):
-        self.browser.quit()
+        assert Elements().check_documents(self.driver)
