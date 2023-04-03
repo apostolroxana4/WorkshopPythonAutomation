fb74220021ee2e69dc6c203502ecdc4740376bf9
Razvan Moisa <razvan.moisa@fortech.ro>
2023-04-03 18:00:52 +0300

    added a scope to the fixture + request so that I can use self in class' functions

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium.webdriver import Chrome
+from driver.driver import Driver
 
 
-@pytest.fixture
-def browser():
+@pytest.fixture(scope='class')
+def browser(request):
     driver = Chrome()
     driver.implicitly_wait(10)
+    driver.get(Driver.URL_demoqa)
+    request.cls.driver = driver
+
     yield driver
     driver.quit()
