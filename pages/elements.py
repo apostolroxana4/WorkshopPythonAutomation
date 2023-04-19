c97e015e698c2dd2c8bd437078da04e4b9bd5445
andreea.cosma <a.cosma@specialized.com>
2023-04-19 14:46:34 +0300

    Last changes

--- pages/elements.py
+++ pages/elements.py
@@ -18,16 +18,16 @@ class StepsElements(Elements):
 
     def fill_credentials(self, browser, username, email, current_address, permanent_address):
         if (
-                (not browser.find_element(By.ID, self.username).visible())
-                or (not browser.find_element(By.XPATH, self.email).visible())
-                or (not browser.find_element(By.CSS_SELECTOR, self.current_address).visible())
-                or (browser.find_element(By.ID, self.permanent_address).visible())
+                (not browser.find_element(By.ID, self.username).is_displayed())
+                or (not browser.find_element(By.XPATH, self.email).is_displayed())
+                or (not browser.find_element(By.CSS_SELECTOR, self.current_address).is_displayed())
+                or (not browser.find_element(By.ID, self.permanent_address).is_displayed())
         ):
             raise ValueError("Fields are not visible")
 
         return (
                 (browser.find_element(By.ID, self.username) == username)
                 and (browser.find_element(By.XPATH, self.email) == email)
-                and (browser.find_element(By.CSS_SELECTOR, self.current_address) == current_address)
-                and (browser.find_element(By.ID, self.permanent_address == permanent_address)
-                     ))
+                #and (browser.find_element(By.CSS_SELECTOR, self.current_address) == current_address)
+                and (browser.find_element(By.ID, self.permanent_address) == permanent_address)
+                     )
