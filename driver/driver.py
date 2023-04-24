72b88ca5378c5d6dbfd3e9cce63182a0aa5d2ec8
Razvan Moisa <razvan.moisa@fortech.ro>
2023-04-24 13:11:55 +0300

    Cleaned the code

--- driver/driver.py
+++ driver/driver.py
@@ -1,13 +1,3 @@
-from selenium.webdriver import Chrome
-
-
 class Driver:
     URL_hekokuapp = "https://the-internet.herokuapp.com/"
     URL_demoqa = "https://demoqa.com/elements"
-
-    @staticmethod
-    def browser_initialization():
-        driver = Chrome()
-        driver.implicitly_wait(10)
-        driver.get(Driver.URL_demoqa)
-        return driver
