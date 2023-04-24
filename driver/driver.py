25c740f410ab6551e5fc4512bd5721c0e266b2d7
Razvan Moisa <razvan.moisa@fortech.ro>
2023-04-24 15:16:43 +0300

    added a new url, for demoqa

--- driver/driver.py
+++ driver/driver.py
@@ -1,3 +1,4 @@
 class Driver:
     URL_hekokuapp = "https://the-internet.herokuapp.com/"
-    URL_demoqa = "https://demoqa.com/elements"
+    URL_demoqa_elements = "https://demoqa.com/elements"
+    URL_demoqa = "https://demoqa.com"
