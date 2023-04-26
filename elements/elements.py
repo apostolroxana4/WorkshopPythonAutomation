2f5a73106e5d4eab141ca379929f56b67186f173
diana.vieriu <diana.vieriu@fortech.ro>
2023-04-26 11:51:20 +0300

    added new elements for checkbox page

--- elements/elements.py
+++ elements/elements.py
@@ -1,7 +1,16 @@
 class Items:
-    elements = "//h5[contains(text(), 'Elements')]"
+    elements = "//*[contains(text(), 'Elements')]"
+
+    check_box = "//*[contains(text(),'Check Box')]"
+    home = "//*[contains(text(),'Home')]"
+    downloads = "//*[contains(text(),'Downloads')]"
+    documents = "//*[contains(text(),'Documents')]"
+    workspace = "//*[contains(text(),'WorkSpace')]"
+    office = "//*[contains(text(),'Office')]"
+    expand_arrow = "svg[class='rct-icon rct-icon-expand-close']"
+    result = 'result'
+    choices = "span[class='text-success']"
+
     radio_button = "//*[contains(text(), 'Radio Button')]"
     yes_radio = "//label[@for='yesRadio']"
     impressive_radio = "//label[@for='impressiveRadio']"
-
-items = Items()
