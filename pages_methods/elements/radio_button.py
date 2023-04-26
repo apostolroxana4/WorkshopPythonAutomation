0f6a359ef660c689136d8f2dd6247cc069b7d4ec
diana.vieriu <diana.vieriu@fortech.ro>
2023-04-26 11:52:45 +0300

    removed static methods

--- pages_methods/elements/radio_button.py
+++ pages_methods/elements/radio_button.py
@@ -1,29 +1,24 @@
 from selenium.webdriver.common.by import By
-from elements.elements import items
+from elements.elements import Items
 
 
 class RadioButton:
+    items = Items()
 
-    @staticmethod
-    def check_radio_button(browser):
-        return browser.find_element(By.XPATH, items.radio_button).is_displayed()
+    def check_radio_button(self, browser):
+        return browser.find_element(By.XPATH, self.items.radio_button).is_displayed()
 
-    @staticmethod
-    def click_radio_button(browser):
-        browser.find_element(By.XPATH, items.radio_button).click()
+    def click_radio_button(self, browser):
+        browser.find_element(By.XPATH, self.items.radio_button).click()
 
-    @staticmethod
-    def check_yes_radio(browser):
-        return browser.find_element(By.XPATH, items.yes_radio).is_displayed()
+    def check_yes_radio(self, browser):
+        return browser.find_element(By.XPATH, self.items.yes_radio).is_displayed()
 
-    @staticmethod
-    def click_yes_radio(browser):
-        browser.find_element(By.XPATH, items.yes_radio).click()
+    def click_yes_radio(self, browser):
+        browser.find_element(By.XPATH, self.items.yes_radio).click()
 
-    @staticmethod
-    def check_impressive_radio(browser):
-        return browser.find_element(By.XPATH, items.impressive_radio).is_displayed()
+    def check_impressive_radio(self, browser):
+        return browser.find_element(By.XPATH, self.items.impressive_radio).is_displayed()
 
-    @staticmethod
-    def click_impressive_radio(browser):
-        browser.find_element(By.XPATH, items.impressive_radio).click()
+    def click_impressive_radio(self, browser):
+        browser.find_element(By.XPATH, self.items.impressive_radio).click()
