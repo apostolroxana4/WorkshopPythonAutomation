bdb7bcd98b544976622d145dac974149d284dc58
diana.vieriu <diana.vieriu@fortech.ro>
2023-04-26 11:58:51 +0300

    added webdriver manager and updated the fixture; added browser as a mandatory argument in order to choose which one should be opened

--- conftest.py
+++ conftest.py
@@ -1,10 +1,27 @@
 import pytest
-from selenium.webdriver import Chrome
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
-@pytest.fixture
-def browser():
-    driver = Chrome()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
+@pytest.fixture(scope="class", autouse=True)
+def setup(request, browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser == 'edge':
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    driver.get('https://demoqa.com/')
+    driver.set_window_position(2000, 0)
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
--- driver/driver.py
+++ driver/driver.py
@@ -1,5 +1,16 @@
 # Here ww will add the driver
-# https://the-internet.herokuapp.com/
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+driver_chrome = webdriver.Chrome(ChromeDriverManager().install())
+driver_edge = webdriver.Edge(EdgeChromiumDriverManager().install())
+
 
 class Driver:
-    URL = 'https://demoqa.com/'
+
+    @staticmethod
+    def open_browser():
+        driver_chrome.get('https://demoqa.com/')
+        driver_chrome.implicitly_wait(10)
+        print(driver_chrome.title)
