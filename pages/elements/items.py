b89b6e7ae5f1ceabb69ab60eccd0c9156ca825a2
Razvan Moisa <razvan.moisa@fortech.ro>
2023-04-27 10:17:12 +0300

    removed empty spaces

--- pages/elements/items.py
+++ pages/elements/items.py
@@ -2,18 +2,12 @@ class Items:
     main_page_elements = "div[class='card mt-4 top-card']"
     section_elements = "//*[contains(text(),'Elements')]"
     subsection_check_box = "//*[contains(text(),'Check Box')]"
-
     home_title = "//*[contains(text(),'Home')]"
-
     documents_title = "//*[contains(text(),'Documents')]"
     documents = "span[class='rct-title']"
-
     office = "//*[contains(text(),'Office')]"
-
     workspace = "//*[contains(text(),'WorkSpace')]"
-
     downloads_title = "//*[contains(text(),'Downloads')]"
-
     clicked_checkboxes = "svg[class ='rct-icon rct-icon-check']"
     result = "span[class ='text-success']"
     arrow = "svg[class='rct-icon rct-icon-expand-close']"
