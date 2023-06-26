db759c45c6a3f9b07ee7da0d597d49187bd86757
Razvan Moisa <razvan.moisa@fortech.ro>
2023-06-26 14:25:42 +0300

    moved functions under a class

--- tests/forms/practice_form/test_fill_form.py
+++ tests/forms/practice_form/test_fill_form.py
@@ -1,77 +1,76 @@
 import pytest
 from selenium.webdriver.common.by import By
-from pages.Practice_Form.elements import Elements
-from pages.Practice_Form.methods import Methods
+from pages.practice_form.elements import Elements
+from pages.practice_form.methods import FormMethods
 
 
-# @pytest.skip(allow_module_level=True)
-def test_click_on_forms(browser):
-    Methods().click_element(browser, By.XPATH, Elements.forms_category)
-    assert Methods().verify_url(browser, "https://demoqa.com/forms")
+# mutat link-urile in variabile + variabile gen label
+# de redus utilizarea la browser
 
 
-def test_click_on_practice_form(browser):
-    Methods().click_element(browser, By.XPATH, Elements.form_subcategory)
-    assert Methods().verify_url(browser, "https://demoqa.com/automation-practice-form")
+@pytest.skip(allow_module_level=True)
+@pytest.mark.usefixtures("browser")
+class TestFillForm:
+    form_methods = FormMethods()
 
+    def test_click_on_forms(self, browser):
+        self.form_methods.click_element(browser, By.XPATH, Elements.forms_category)
+        assert self.form_methods.verify_url(browser, "https://demoqa.com/forms")
 
-def test_fill_name(browser):
-    Methods().fill(browser, By.ID, Elements.first_name, 'Bob')
-    assert Methods().validate_field(browser, By.ID, Elements.first_name, "Bob")
+    def test_click_on_practice_form(self, browser):
+        self.form_methods.click_element(browser, By.XPATH, Elements.form_subcategory)
+        assert self.form_methods.verify_url(browser, "https://demoqa.com/automation-practice-form")
 
-    Methods().fill(browser, By.ID, Elements.last_name, 'The Builder')
-    assert Methods().validate_field(browser, By.ID, Elements.last_name, 'The Builder')
+    def test_fill_name(self, browser):
+        self.form_methods.fill(browser, By.ID, Elements.first_name, 'Bob')
+        assert self.form_methods.validate_field(browser, By.ID, Elements.first_name, "Bob")
 
+        self.form_methods.fill(browser, By.ID, Elements.last_name, 'The Builder')
+        assert self.form_methods.validate_field(browser, By.ID, Elements.last_name, 'The Builder')
 
-def test_select_gender(browser):
-    Methods().click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.gender_male}']")
-    assert Methods().is_element_selected(browser, By.ID, Elements.gender_male)
+    def test_select_gender(self, browser):
+        self.form_methods.click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.gender_male}']")
+        assert self.form_methods.is_element_selected(browser, By.ID, Elements.gender_male)
 
+    def test_fill_mobile_nr(self, browser):
+        # make a function that encapsulates the insertion of a wrong number
 
-def test_fill_mobile_nr(browser):
-    Methods().click_element(browser, By.ID, Elements.mobile_number)
-    Methods().fill(browser, By.ID, Elements.mobile_number, 'wrong number')
-    Methods().clear_field(browser, By.ID, Elements.mobile_number)
-    Methods().fill(browser, By.ID, Elements.mobile_number, '0722333446')
-    assert Methods().validate_field(browser, By.ID, Elements.mobile_number, '0722333446')
+        self.form_methods.click_element(browser, By.ID, Elements.mobile_number)
+        self.form_methods.fill(browser, By.ID, Elements.mobile_number, 'wrong number')
+        self.form_methods.clear_field(browser, By.ID, Elements.mobile_number)
+        self.form_methods.fill(browser, By.ID, Elements.mobile_number, '0722333446')
+        assert self.form_methods.validate_field(browser, By.ID, Elements.mobile_number, '0722333446')
 
+    def test_fill_calendar(self, browser):
+        self.form_methods.clear_calendar_date(browser, By.ID, Elements.date_of_birth)
+        self.form_methods.fill(browser, By.ID, Elements.date_of_birth, "3 Sep 2023")
+        assert self.form_methods.validate_field(browser, By.ID, Elements.date_of_birth, "23 Sep 2023")
 
-def test_fill_calendar(browser):
-    Methods().clear_calendar_date(browser, By.ID, Elements.date_of_birth)
-    Methods().fill(browser, By.ID, Elements.date_of_birth, "3 Sep 2023")
-    assert Methods().validate_field(browser, By.ID, Elements.date_of_birth, "23 Sep 2023")
+    def test_select_hobbies(self, browser):
+        self.form_methods.click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.hobbies_sports}']")
+        assert self.form_methods.is_element_selected(browser, By.ID, Elements.hobbies_sports)
 
+        self.form_methods.click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.hobbies_music}']")
+        assert self.form_methods.is_element_selected(browser, By.ID, Elements.hobbies_music)
 
-def test_select_hobbies(browser):
-    Methods().click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.hobbies_sports}']")
-    assert Methods().is_element_selected(browser, By.ID, Elements.hobbies_sports)
+    def test_scroll_to_the_bottom(self, browser):
+        self.form_methods.scroll_to_the_bottom(browser)
+        assert self.form_methods.is_element_displayed(browser, By.ID, Elements.state)
 
-    Methods().click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.hobbies_music}']")
-    assert Methods().is_element_selected(browser, By.ID, Elements.hobbies_music)
+    def test_select_state(self, browser):
+        self.form_methods.click_element(browser, By.ID, Elements.state)
+        self.form_methods.click_element(browser, By.XPATH, Elements.haryana)
+        # assert Methods().validate_field(browser, By.XPATH, Elements.haryana, "Haryana")
 
+    def test_select_city(self, browser):
+        self.form_methods.click_element(browser, By.ID, Elements.city)
+        self.form_methods.click_element(browser, By.ID, Elements.karnal)
 
-def test_scroll_to_the_bottom(browser):
-    Methods().scroll_to_the_bottom(browser)
-    assert Methods().is_element_displayed(browser, By.ID, Elements.state)
+    def test_submit_form(self, browser):
+        self.form_methods.submit_form(browser, By.ID, Elements.form)
+        self.form_methods.maximize_window(browser)
+        assert self.form_methods.is_element_displayed(browser, By.CSS_SELECTOR, Elements.modal_form_result)
 
-
-def test_select_state(browser):
-    Methods().click_element(browser, By.ID, Elements.state)
-    Methods().click_element(browser, By.XPATH, Elements.haryana)
-    # assert Methods().validate_field(browser, By.XPATH, Elements.haryana, "Haryana")
-
-
-def test_select_city(browser):
-    Methods().click_element(browser, By.ID, Elements.city)
-    Methods().click_element(browser, By.ID, Elements.karnal)
-
-
-def test_submit_form(browser):
-    Methods().submit_form(browser, By.ID, Elements.form)
-    Methods().maximize_window(browser)
-    assert Methods().is_element_displayed(browser, By.CSS_SELECTOR, Elements.modal_form_result)
-
-
-def test_close_modal(browser):
-    Methods().close_modal(browser, By.ID, Elements.close_modal)
-    assert Methods().verify_url(browser, 'https://demoqa.com/automation-practice-form')
+    def test_close_modal(self, browser):
+        self.form_methods.close_modal(browser, By.ID, Elements.close_modal)
+        assert self.form_methods.verify_url(browser, 'https://demoqa.com/automation-practice-form')
--- tests/forms/practice_form/test_refresh_form.py
+++ tests/forms/practice_form/test_refresh_form.py
@@ -1,34 +1,34 @@
 import pytest
 from selenium.webdriver.common.by import By
-from pages.Practice_Form.elements import Elements
-from pages.Practice_Form.methods import Methods
+from pages.practice_form.elements import Elements
+from pages.practice_form.methods import FormMethods
 
 
 # @pytest.skip(allow_module_level=True)
-def test_click_on_forms(browser):
-    Methods().click_element(browser, By.XPATH, Elements.forms_category)
-    assert Methods().verify_url(browser, "https://demoqa.com/forms")
+@pytest.mark.usefixtures("browser")
+class TestRefreshForm:
+    form_methods = FormMethods()
 
+    def test_click_on_forms(self, browser):
+        self.form_methods.click_element(browser, By.XPATH, Elements.forms_category)
+        assert self.form_methods.verify_url(browser, "https://demoqa.com/forms")
 
-def test_click_on_practice_form(browser):
-    Methods().click_element(browser, By.XPATH, Elements.form_subcategory)
-    assert Methods().verify_url(browser, "https://demoqa.com/automation-practice-form")
+    def test_click_on_practice_form(self, browser):
+        self.form_methods.click_element(browser, By.XPATH, Elements.form_subcategory)
+        assert self.form_methods.verify_url(browser, "https://demoqa.com/automation-practice-form")
 
+    def test_fill_name(self, browser):
+        self.form_methods.fill(browser, By.ID, Elements.first_name, 'Bob')
+        assert self.form_methods.validate_field(browser, By.ID, Elements.first_name, "Bob")
 
-def test_fill_name(browser):
-    Methods().fill(browser, By.ID, Elements.first_name, 'Bob')
-    assert Methods().validate_field(browser, By.ID, Elements.first_name, "Bob")
+        self.form_methods.fill(browser, By.ID, Elements.last_name, 'The Builder')
+        assert self.form_methods.validate_field(browser, By.ID, Elements.last_name, 'The Builder')
 
-    Methods().fill(browser, By.ID, Elements.last_name, 'The Builder')
-    assert Methods().validate_field(browser, By.ID, Elements.last_name, 'The Builder')
+    def test_select_gender(self, browser):
+        self.form_methods.click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.gender_male}']")
+        assert self.form_methods.is_element_selected(browser, By.ID, Elements.gender_male)
 
-
-def test_select_gender(browser):
-    Methods().click_element(browser, By.CSS_SELECTOR, f"label[for='{Elements.gender_male}']")
-    assert Methods().is_element_selected(browser, By.ID, Elements.gender_male)
-
-
-def test_refresh_and_confirm(browser):
-    Methods().refresh_page(browser)
-    assert browser.find_element(By.ID, "firstName").get_attribute("value") == ''
-    assert not browser.find_element(By.ID, "gender-radio-1").is_selected()
+    def test_refresh_and_confirm(self, browser):
+        self.form_methods.refresh_page(browser)
+        assert browser.find_element(By.ID, "firstName").get_attribute("value") == ''
+        assert not browser.find_element(By.ID, "gender-radio-1").is_selected()
