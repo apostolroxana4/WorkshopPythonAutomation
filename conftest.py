32cb715dd1373dab69231d2d550bbd69f031aee4
Razvan Moisa <razvan.moisa@fortech.ro>
2023-06-27 09:15:39 +0300

    changed conftest, replaced the previous zoom script with another one that works

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium.webdriver import Chrome
 from driver.driver import Driver
@@ -8,8 +10,7 @@ def browser():
     driver = Chrome()
     driver.implicitly_wait(10)
     driver.get(Driver.URL_demoqa)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    driver.execute_script("document.body.style.zoom='80%'")
+    driver.execute_script("document.body.style['-webkit-transform'] = \"scale(0.8)\";")
 
     yield driver
     driver.quit()
