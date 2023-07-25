c98c23aa9ef9f7f04116554f449edcd3badb46b8
Razvan <moisa.razvan13@gmail.com>
2023-07-25 17:26:39 +0300

    finished the raw version, containing post, get, delete, get

--- backend_requests.py
+++ backend_requests.py
@@ -1,6 +1,6 @@
-import curlify
 import requests
 import random
+import pprint
 
 
 class BackendRequests:
@@ -9,6 +9,8 @@ class BackendRequests:
     authorize_account_url = "https://demoqa.com/Account/v1/Authorized"
     post_book_url = "https://demoqa.com/BookStore/v1/Books"
     userID = "b5d1f0f7-6bac-4f5c-9be6-4437f506f8e2"
+    get_user_details_url = "https://demoqa.com/Account/v1/User/"+userID
+    delete_books_url = "https://demoqa.com/BookStore/v1/Books?UserId="+userID
     name = "razvan"
     password = "123Abcdefg@"
     token = ""
@@ -16,20 +18,19 @@ class BackendRequests:
         "userName": name,
         "password": password
     }
-    collectionOfIsbns = []
+
     book_list_id = []
 
     def generate_books(self):
+        collectionOfIsbns = []
         books_get = requests.get(self.get_books_url)
         for i in range(len(books_get.json()["books"])):
             self.book_list_id.append(books_get.json()["books"][i]['isbn'])
-        number_of_books = random.randint(1, 8)
 
-        for i in range(number_of_books):
-            random.shuffle(self.book_list_id)
-            self.collectionOfIsbns.append(self.book_list_id.pop())
-        print(self.collectionOfIsbns)
-        return self.collectionOfIsbns
+
+        random.shuffle(self.book_list_id)
+        collectionOfIsbns.append(self.book_list_id.pop())
+        return collectionOfIsbns
 
     def authorize_account(self):
         post_authorize_account = requests.post(self.authorize_account_url, self.authorize_body)
@@ -49,16 +50,24 @@ class BackendRequests:
                 }
             ]
         }
-        headers = {"Authorization": "Bearer " + self.token,
-                   # "Content-Type": "application/json",
-                   "User-Agent": "foo",
-                   "accept": "application/json"}
-        post_book_request = requests.post(self.post_book_url, post_book_body, headers=headers, stream=True, timeout=30)
-        return post_book_request
+        headers = {"Authorization": "Bearer " + self.token}
+        requests.post(self.post_book_url, json=post_book_body, headers=headers, timeout=30)
 
+    def get_user_details(self):
+        headers = {"Authorization": "Bearer " + self.token}
+        get_user_info = requests.get(self.get_user_details_url, headers=headers)
+        pprint.pprint(get_user_info.json()["books"])
+
+    def delete_books(self):
+        headers = {"Authorization": "Bearer " + self.token}
+        requests.delete(self.delete_books_url, headers=headers)
 
 backend_requests = BackendRequests()
 backend_requests.authorize_account()
 backend_requests.generate_token()
-print(curlify.to_curl(backend_requests.authorize_account().request))
 backend_requests.post_books()
+backend_requests.post_books()
+
+backend_requests.get_user_details()
+backend_requests.delete_books()
+backend_requests.get_user_details()
