da08552c1e3f9481b77442746d420a8b337b6e89
apostolroxana4 <112384693+apostolroxana4@users.noreply.github.com>
2023-09-26 18:25:57 +0300

    Update conftest.py for testing github actions

--- conftest.py
+++ conftest.py
@@ -8,3 +8,13 @@ def browser():
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
+
+
+print("Hello word!")
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
